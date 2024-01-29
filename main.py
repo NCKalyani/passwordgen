ad1c7c78681c832f16fb7bb8a9bb09e918713a80
kalyani <n.chandra.kalyani@gmail.com>
2024-01-29 16:25:36 +0530

    pass generator initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# password generator
+import random
+import string
+total = string.ascii_uppercase+string.digits+string.punctuation+string.ascii_lowercase
+length = 16
+password =''.join(random.sample(total,length))
+print(password)
